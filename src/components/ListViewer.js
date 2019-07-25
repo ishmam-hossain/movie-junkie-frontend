a4950092641d31ad7ce203e49762713bc580d6f9
ishmam_tonmoy <ishmam@finder-lbs.com>
2019-07-26 03:02:57 +0600

    added download and summary button --initial stage

--- src/components/ListViewer.js
+++ src/components/ListViewer.js
@@ -1,25 +1,57 @@
 import React from 'react'
-import { Card, Icon } from 'antd';
+import { Card, Icon, Popover, Button } from 'antd';
 const { Meta } = Card;
 
 export class ListViewer extends React.Component {
 
+    downloadTorrent(downloadUrl) {
+        console.log(downloadUrl);
+    }
+
+
     render() {
         let movieList = this.props.movieList;
         let movies = movieList.data.movies;
+
+        const content = (content) => (
+            <div>
+                <p>{content}</p>
+            </div>
+        );
+
+        const downloaderView = (torrents) => (
+            <div>
+                <Button>{torrents[0].quality}</Button>
+                <Button>{torrents[1].quality}</Button>
+            </div>
+        );
+
+        const downloadButton = (urls) => (
+            <Popover content={content(urls[0])} title={urls[1]}>
+                <Button type="default">Summary</Button>
+            </Popover>
+        );
+        
 
         let summaryList = movies.map(movie => {
             return (
                 <Card className="List" key={movie.id}
                     hoverable
                     style={{ width: 240 }}
-                    cover={<img src={movie.medium_cover_image} alt="Movie Cover"/>}
-                    actions={[<Icon type="download"/>]}
+                    cover={<img src={movie.medium_cover_image} alt={movie.title} />}
                 >
-                    <Meta
-                        title={movie.title}
-                        description={movie.summary}
-                    />
+
+                    <Meta title={movie.title}/>
+
+                    <br/>
+
+                    <Popover content={content(movie.summary)} title={movie.title}>
+                        <Button type="default">Summary</Button>
+                    </Popover>
+
+                    <Popover content={downloaderView(movie.torrents)} title={movie.title}>
+                        <Button type="default">Download</Button>
+                    </Popover>
 
                 </Card>);
         })
@@ -31,3 +63,7 @@ export class ListViewer extends React.Component {
         );
     }
 }
+
+
+// download torrent
+// fix the summary issue
